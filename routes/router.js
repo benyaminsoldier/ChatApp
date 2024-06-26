d44af101f42c9cb6c1cae15c8bfc49e096f112bd
benyaminsoldier <daniel.benjumea0@gmail.com>
2024-06-26 08:52:03 -0600

    checking on render

--- routes/router.js
+++ routes/router.js
@@ -4,6 +4,8 @@ const middlewares = require('../controls/middlewares.js')
 
 router.use('/', (req,res)=>{
     console.log('hi this is the router responding by default')
+    console.log('Request method:', req.method)
+    console.log('Request headers:', req.headers)
     console.log(req.body)
 })
 
