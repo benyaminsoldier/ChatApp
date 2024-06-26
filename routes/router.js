3ce8466a4e327e15cd379dbecfc05e6209244125
benyaminsoldier <daniel.benjumea0@gmail.com>
2024-06-26 11:27:00 -0600

    head route removed

--- routes/router.js
+++ routes/router.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 const middlewares = require('../controls/middlewares.js')
 
-
+/*
 router.head('/', (req, res) => {
     console.log('HEAD request received');
     //res.sendStatus(200);  // Send a 200 OK status without a body
-});
+});*/
 
 // Handle GET requests to the root route
 router.get('/', (req, res) => {
